943ef3f5e01b6d1fe6db400f28e9418455dce92a
João Paulo Souza Bernucio <joaobernucio@proton.me>
2024-09-19 21:21:45 -0300

    Manipulando Strings com Python

---
+++ string_1.py
@@ -0,0 +1,17 @@
+nome = "joãO PaULo"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = " Olá, Mundo!  "
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.lstrip() + ".")
+print(texto.rstrip() + ".")
+
+menu = "Python"
+print(menu.center(14))
+print(menu.center(14, "#"))
+print("p-y-t-h-o-n")
+print("-".join(menu))
