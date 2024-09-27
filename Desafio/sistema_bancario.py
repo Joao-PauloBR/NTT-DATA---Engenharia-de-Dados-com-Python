cecd9acfc081ee4c1c7bc25e0b3f96bffcf9b0e9
João Paulo Souza Bernucio <joaobernucio@proton.me>
2024-09-27 07:07:40 -0300

    Sistema Bancário 2.7

--- Desafio/sistema_bancario.py
+++ Desafio/sistema_bancario.py
@@ -10,13 +10,6 @@ print('''
 ''')
 
 # Variáveis existentes para transações financeiras
-saldo = 1500
-numero_saques = 0
-numero_depositos = 0
-lista_de_saques = []
-lista_de_depositos = []
-data_hora_saque = []
-data_hora_deposito = []
 mascara_ptbr = "%d/%m/%Y %H:%M"
 quantidade_transacao = 10
 
@@ -65,7 +58,14 @@ def cadastrar_conta():
         conta = {
             "agencia": "0001",
             "numero_conta": numero_conta_sequencial,
-            "usuario": usuario_encontrado
+            "usuario": usuario_encontrado,
+            "saldo": 0.0,
+            "numero_saques": 0,
+            "numero_depositos": 0,
+            "lista_de_saques": [],
+            "lista_de_depositos": [],
+            "data_hora_saque": [],
+            "data_hora_deposito": []
         }
         contas.append(conta)
         numero_conta_sequencial += 1
@@ -89,6 +89,22 @@ def listar_contas():
         for conta in contas:
             print(f"Agência: {conta['agencia']}, Conta: {conta['numero_conta']}, Usuário: {conta['usuario']['nome']}")
 
+def buscar_conta_por_usuario():
+    cpf = input("Digite o CPF do usuário: ")
+
+    # Verificar se o usuário existe
+    conta_encontrada = None
+    for conta in contas:
+        if conta["usuario"]["cpf"] == cpf:
+            conta_encontrada = conta
+            break
+    
+    if conta_encontrada:
+        return conta_encontrada
+    else:
+        print("Erro: Conta não encontrada.")
+        return None
+
 # Modificação da função que controla as transações para verificar se há usuários e contas cadastrados
 def verificar_usuarios_e_contas():
     if len(usuarios) == 0 or len(contas) == 0:
@@ -100,96 +116,85 @@ def depositar():
     if not verificar_usuarios_e_contas():
         return
     
-    global saldo
-    global lista_de_depositos
-    global quantidade_transacao
-    global numero_saques
-    global numero_depositos
-    if numero_saques + numero_depositos < 10:
-        deposito = float(input("Digite o valor do depósito: "))
-        if deposito >= 0.01:
-            saldo += deposito
-            print(f"Depósito realizado com sucesso. O valor de R$ {deposito:.2f} foi transferido.")
-            print(f"Seu saldo atual é R$ {saldo:.2f}")
-            lista_de_depositos.append(deposito)
-            data_hora_deposito.append(datetime.today())
-            numero_depositos += 1
-        elif deposito <= 0:
-            print(f"Você não pode depositar R$ {deposito:.2f}. Insira um valor válido.")
+    conta = buscar_conta_por_usuario()
+    if conta:
+        if conta["numero_saques"] + conta["numero_depositos"] < 10:
+            deposito = float(input("Digite o valor do depósito: "))
+            if deposito >= 0.01:
+                conta["saldo"] += deposito
+                print(f"Depósito realizado com sucesso. O valor de R$ {deposito:.2f} foi transferido.")
+                print(f"Seu saldo atual é R$ {conta['saldo']:.2f}")
+                conta["lista_de_depositos"].append(deposito)
+                conta["data_hora_deposito"].append(datetime.today())
+                conta["numero_depositos"] += 1
+            else:
+                print(f"Você não pode depositar R$ {deposito:.2f}. Insira um valor válido.")
         else:
-            print("Erro de sistema. Tente novamente mais tarde.")
-    else:
-        print("Você excedeu o número de transações permitidas. Volte amanhã para realizar mais operações.")
+            print("Você excedeu o número de transações permitidas. Volte amanhã para realizar mais operações.")
 
 def sacar():
     if not verificar_usuarios_e_contas():
         return
     
-    global saldo
-    global numero_saques
-    global lista_de_saques
-    global numero_depositos
-    if numero_saques + numero_depositos < 10:    
-        if numero_saques < 3:
-            saque = float(input("Digite o valor que deseja sacar: "))
-            if saque <= saldo and saque >= 0.01:
-                if saque <= 500:
-                    saldo -= saque
-                    print(f"Saque efetuado com sucesso. Você sacou R$ {saque:.2f}. Seu saldo atual é de R$ {saldo:.2f}")
-                    lista_de_saques.append(saque)
-                    data_hora_saque.append(datetime.today())
-                    numero_saques += 1
-                elif saque > 500:
-                    print("Você possui saldo suficiente, porém não podemos permitir saques maiores do que R$ 500.00")
-            elif saque > saldo:
-                print(f"ERRO! Você está tentando sacar um valor maior do que possui de saldo. Seu saldo atual é R$ {saldo:.2f}")
+    conta = buscar_conta_por_usuario()
+    if conta:
+        if conta["numero_saques"] + conta["numero_depositos"] < 10:    
+            if conta["numero_saques"] < 3:
+                saque = float(input("Digite o valor que deseja sacar: "))
+                if saque <= conta["saldo"] and saque >= 0.01:
+                    if saque <= 500:
+                        conta["saldo"] -= saque
+                        print(f"Saque efetuado com sucesso. Você sacou R$ {saque:.2f}. Seu saldo atual é de R$ {conta['saldo']:.2f}")
+                        conta["lista_de_saques"].append(saque)
+                        conta["data_hora_saque"].append(datetime.today())
+                        conta["numero_saques"] += 1
+                    else:
+                        print("Você possui saldo suficiente, porém não podemos permitir saques maiores do que R$ 500.00")
+                elif saque > conta["saldo"]:
+                    print(f"ERRO! Você está tentando sacar um valor maior do que possui de saldo. Seu saldo atual é R$ {conta['saldo']:.2f}")
+                else:
+                    print(f"Você não pode sacar R$ {saque:.2f}. Insira um valor válido.")
             else:
-                print(f"Você não pode sacar R$ {saque:.2f}. Insira um valor válido.")
+                print("Você já realizou três saques hoje. Por segurança, volte amanhã se deseja sacar novamente.")
         else:
-            print("Você já realizou três saques hoje. Por segurança, volte amanhã se deseja sacar novamente.")
-    else:
-        print("Você excedeu o número de transações permitidas. Volte amanhã para realizar mais operações.")
+            print("Você excedeu o número de transações permitidas. Volte amanhã para realizar mais operações.")
 
 def imprimir_extrato():
     if not verificar_usuarios_e_contas():
         return
     
-    global saldo
-    global lista_de_depositos
-    global lista_de_saques
-    global data_hora_saque
-    global data_hora_deposito
-    global mascara_ptbr
+    conta = buscar_conta_por_usuario()
+    if conta:
+        # DEPÓSITOS
+        if len(conta["lista_de_depositos"]) == 0:
+            print("Não há depósitos realizados.")
+        else:
+            print("Depósitos realizados na sua conta")
 
-    # DEPÓSITOS
-    if len(lista_de_depositos) == 0:
-        print("Não há depósitos realizados.")
-    else:
-        print("Depósitos realizados na sua conta")
-
-    for data, deposito in zip(data_hora_deposito, lista_de_depositos):
-        data_formatada = data.strftime(mascara_ptbr)
-        print(f'''
+        for data, deposito in zip(conta["data_hora_deposito"], conta["lista_de_depositos"]):
+            data_formatada = data.strftime(mascara_ptbr)
+            print(f'''
 {data_formatada}
 R$ {deposito:.2f}''')
         
-    print("=" * 30)
+        print("=" * 30)
 
-    # SAQUES
-    if len(lista_de_saques) == 0:
-        print("Não há saques realizados.")
-    else:    
-        print("Saques realizados na sua conta")
+        # SAQUES
+        if len(conta["lista_de_saques"]) == 0:
+            print("Não há saques realizados.")
+        else:    
+            print("Saques realizados na sua conta")
 
-    for data, saque in zip(data_hora_saque, lista_de_saques):
-        data_formatada = data.strftime(mascara_ptbr)
-        print(f'''
+        for data, saque in zip(conta["data_hora_saque"], conta["lista_de_saques"]):
+            data_formatada = data.strftime(mascara_ptbr)
+            print(f'''
 {data_formatada}
 R$ {saque:.2f}''')
 
-    print("=" * 30)
+        print("=" * 30)
 
-    print(f"Saldo atual da sua conta: R$ {saldo:.2f}")
+        print(f"Saldo atual da sua conta: R$ {conta['saldo']:.2f}")
+
 
 # Adicionar as novas opções no menu
 def menu():
